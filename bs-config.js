dd8d15ec3c2947c85c1431379a4e5879e2825abc
Ishan Bagul <bagul.mahendra@oyorooms.com>
2021-06-11 03:37:46 +0530

    config fix

---
+++ bs-config.js
@@ -0,0 +1,8 @@
+module.exports = {
+    port: process.env.PORT,
+    files: ['./**/*.{html,htm,css,js}'],
+    server:{
+        baseDir: "./"
+    },
+    proxy : "https://blockchain-ari.herokuapp.com:3001"
+};
